c1baddf78c37cb29e0b18f2bbc06968ea75a966a
Dmitrii Alekseev <scoolboy1928@gmail.com>
2023-12-16 11:53:43 +0300

    Initial commit

---
+++ src/main/java/com/r3negade/practic2web/Practic2webApplication.java
@@ -0,0 +1,19 @@
+package com.r3negade.practic2web;
+
+import com.r3negade.practic2web.models.Model1;
+import com.r3negade.practic2web.repos.Model1Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practic2webApplication {
+
+
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Practic2webApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/r3negade/practic2web/Practic2webApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r3negade.practic2web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practic2webApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
